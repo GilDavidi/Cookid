8a86b7c8a72308093fda3d0c15af9d1ef8dd0e9e
guybardor <1993gbd@gmail.com>
2022-12-26 11:03:24 +0200

    gil and i  start build a server

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,7 +2,9 @@ const path = require("path");
 
 module.exports = {
     loadGame: (req, res) => {
-        res.sendFile(path.join(__dirname, '../game/view/MissionCanvas.html'));
+        //res.sendFile(path.join(__dirname, '../game/view/MissionCanvas.html'));
+        console.log("load game is good");
+        res.send('localhost:5000/');
     },
 
 }
