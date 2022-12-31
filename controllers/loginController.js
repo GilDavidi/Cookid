5a8c8b79aedf858ef8d136c108418ddbff20b1ed
Tomeref4 <tomer.ifargan@stu.shenkar.ac.il>
2022-12-31 21:07:00 +0200

    Update

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,16 +1,18 @@
 const User = require('../mongoDB/models/users');
+const loggedUsers = {};
 module.exports = {
     checkUserTeacher: (req, res) => {
         User.findOne({'user_name': req.body.userName, 'password': req.body.password})
         .then(result => {
                 if (result) {
+                    console.log(result.id);
                     res.send("The user exist");
                 } else {
                     res.send("The user does not exist, try again");
                 }
             })
                 .catch(err => console.log(err));
-        },
+    },
     checkUserPupil: (req, res) => {
         User.findOne({'user_name': req.body.userName})
             .then(result => {
